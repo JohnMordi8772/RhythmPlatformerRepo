802b22cbad1ade24e385b9e7664470a4269bd92d
tcolander <tcolander@mail.bradley.edu>
2022-02-22 07:03:11 -0600

    Update TylerGameController.cs

--- RhythmPlatformPrototype/Assets/Scripts/TylerIsGoingOffTheDeepEnd/TylerGameController.cs
+++ RhythmPlatformPrototype/Assets/Scripts/TylerIsGoingOffTheDeepEnd/TylerGameController.cs
@@ -13,6 +13,7 @@ public class TylerGameController : MonoBehaviour
     public AudioClip wrongSound;
     public GameObject ArrowSpawner;
     public int i, j;
+
     //yes, this was unfortunately necessary
     public AudioClip A2, A3, A4, B2, B3, B4, C3, C4, C5, D3, D4, D5, E3, E4, F3, F4, G3, G4, Ab3, Ab4, Bb2, Bb3, Bb4, Db3, Db4, Db5, Eb3, Eb4, Gb3, Gb4;
 
@@ -27,6 +28,8 @@ public class TylerGameController : MonoBehaviour
 
     public void Awake()
     {
+
+        //This maps 
         mapSoundArray = new AudioClip[64] { D4, D4, D5, null, A4, null, null, Ab4, null, G4, null, F4, null, D4, F4, G4, 
             C4, C4, D5, null, A4, null, null, Ab4, null, G4, null, F4, null, D4, F4, G4, 
             B3, B3, D5, null, A4, null, null, Ab4, null, G4, null, F4, null, D4, F4, G4, 
@@ -34,6 +37,7 @@ public class TylerGameController : MonoBehaviour
         //null are pauses, The rest are musical notes
 
 
+        //this maps arrows and obstacles in eighth note increments
         mapObjectArray = new int[64]      {  3,  3,  1,    0,  2,    0,   5,   4,    0,  2,    5,  3,    0,  2,  1,  4, 
             2, 2, 1, 0, 3, 0, 5, 4, 0, 2, 5, 3, 0, 2, 1, 4, 
             3, 3, 1, 0, 2, 0, 5, 4, 0, 2, 5, 3, 0, 2, 1, 4, 
@@ -46,7 +50,10 @@ public class TylerGameController : MonoBehaviour
     }
     private void Start()
     {
+        //this starts as the arrows begin to spawn
         InvokeRepeating("TimeToChangeArrowTrue", 4f, .25f);
+
+        //this starts as the first arrow gets to the player.
         InvokeRepeating("TimeToChangeSoundTrue", 5.5f, .25f);
 
     }
